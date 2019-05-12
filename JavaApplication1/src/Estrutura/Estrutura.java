3a420b86d11e7521eee89e720841adb9fb5862f2
João P. Barberino <joao.p.barberino@gmail.com>
2019-05-12 20:07:57 -0300

    Alterações e progresso nas Estruturas, comentado

--- JavaApplication1/src/Estrutura/Estrutura.java
+++ JavaApplication1/src/Estrutura/Estrutura.java
@@ -5,7 +5,9 @@
  */
 package Estrutura;
 
+import javaapplication1.Inimigo;
 import java.util.*;
+import mapa.*;
 
 /**
  *
@@ -16,59 +18,78 @@ public class Estrutura {
     private int ataque;
     private int alcance;
     private int velo_atk;
-    private int round;
-//    private ArrayList<No> alcance_de_ataque = new ArrayList();
-//    private No pos;
-//    private Mapa mapa;
+    private int pos;
+    private int nivel = 1;
+    private ArrayList<Integer> casas_no_alcance = new ArrayList();
+    private Mapa mapa;
 
-    public Estrutura(int ataque, int alcance, int velo_atk, int round) {
+    public Estrutura(int ataque, int alcance, int velo_atk, int pos) {
         this.ataque = ataque;
         this.alcance = alcance;
         this.velo_atk = velo_atk;
-        this.round = round;
-       // this.mapa = mapa;
-    }
-    
-    public void print(){
-        System.out.println(this.round);
+        this.pos = pos;
     }
 
-//    public void construir(No pos) {
+//  
+//    public void construir(int pos) {
 //        // desenha na tela
-//        setRange();
+//        // poe no objeto mapa pelo main
 //    }
-//
-//    private void setRange(Mapa mapa) {
-//        int x;
-//        this.alcance_de_ataque.add(mapa.getMapa.get(x));
-//    }
-
+//      
+    // Retorna o alcance de ataque da estrutura
     public int getAlcance() {
         return alcance;
     }
-
+    
+    // Modifica o alcance da estrutura
     public void setAlcance(int alcance) {
         this.alcance = alcance;
+        //this.setRange();
     }
-
+    
+    // Retorna a velocidade de ataque da estrutura
     public int getVelo_atk() {
         return velo_atk;
     }
-
+    
+    // Modifica a velocidade de ataque da estrutura
     public void setVelo_atk(int velo_atk) {
         this.velo_atk = velo_atk;
     }
-
+    
+    // Retorna o valor de ataque da estrutura
     public int getAtaque() {
         return ataque;
     }
-
+    
+    // Modifica o valor de ataque da estrutura
     public void setAtaque(int ataque) {
         this.ataque = ataque;
     }
 
-    public void atacar(Object inimigo) {
-
+    // Ataca um dado inimigo, reduzindo sua vida
+    public void atacar(Inimigo inimigo) {
+        inimigo.reduzVida(this.ataque);
     }
 
+    // Da um mapa pra estrutura e calcula as casas que ela pode atacar
+    public void setMapa(Mapa mapa) {
+        this.mapa = mapa;
+        //this.setRange();
+    }
+    
+    // Aumenta o nivel da torre
+    public void update(){
+        this.nivel ++;
+        this.ataque ++;
+        this.alcance ++;
+        //this.setRange();    
+    }
+    
+//    responsável por calcular quais posições do mapa estão no alcance da torre
+//    private void setRange() {
+//        int x;
+//        this.casas_no_alcance.add(this.mapa.getMapa.get(x));
+//    }
+//   
 }
