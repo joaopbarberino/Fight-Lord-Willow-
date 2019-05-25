31212d153ea222c9fabce7c9fbb9c8d72d237ea7
pedroaredes <49043940+pedroaredes@users.noreply.github.com>
2019-05-25 20:17:39 -0300

    Create Engine.java

---
+++ JavaApplication1/src/javaapplication1/Engine.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PEDROKREIDABALA
+ */
+public class Engine {
+    public static final int TILE_WIDTH = 50;
+    public static final int TILE_HEIGTH = 50;
+}
