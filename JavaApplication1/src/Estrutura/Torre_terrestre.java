f8de0079aad391aad8ff2435cd07386b418d84bf
João P. Barberino <joao.p.barberino@gmail.com>
2019-06-08 23:04:26 -0300

    add qtd colunas ao engine

--- JavaApplication1/src/Estrutura/Torre_terrestre.java
+++ JavaApplication1/src/Estrutura/Torre_terrestre.java
@@ -8,6 +8,7 @@ package Estrutura;
 import javaapplication1.Desenhavel;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import javaapplication1.Engine;
 
 /**
  *
@@ -16,15 +17,17 @@ import java.awt.image.BufferedImage;
 public class Torre_terrestre extends Estrutura implements Desenhavel {
 
     private final BufferedImage SPRITE;
+    private int x, y;
 
     public Torre_terrestre(int pos, BufferedImage sprite) {
         super(1, 3, 3, pos);
         this.SPRITE = sprite;
+        this.x = ((posAtual / Engine.QTD_COLUNAS) * Engine.TILE_SIZE);
     }
 
     @Override
     public void paintComponent(Graphics g) {
-
+        g.drawImage(SPRITE.getSubimage(0, 0, 40, 40),,, this)
     }
 
 }
